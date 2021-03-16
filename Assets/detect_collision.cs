ed28cb3ea1d4ff071413c75b214eec8ecb236a44
mdlaras <mariadlarasati@gmail.com>
2021-03-16 17:14:43 +0700

    add life display

--- Assets/detect_collision.cs
+++ Assets/detect_collision.cs
@@ -7,6 +7,11 @@ public class detect_collision : MonoBehaviour
     [SerializeField] float collision_boundary;
     [SerializeField] make_child childmaker;
     [SerializeField] bool is_asteroid;
+    [SerializeField] live live;
+    [SerializeField] float collision_timespan;
+    bool collided=false;
+    float collision_time = 0;
+
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +21,12 @@ public class detect_collision : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(Time.time - collision_time);
+        if (Time.time - collision_time > collision_timespan && collided==true)
+        {
+            collided = false;
+        }
+
         if (is_asteroid is true)
         {
             var projectiles = FindObjectsOfType<projectile_move>();
@@ -36,9 +47,11 @@ public class detect_collision : MonoBehaviour
             foreach (asteroid_move asteroid in asteroids)
             {
                 var asteroid_relative_position = Mathf.Pow(asteroid.transform.position.x - transform.position.x, 2) + Mathf.Pow(asteroid.transform.position.y - transform.position.y, 2);
-                if (asteroid_relative_position < collision_boundary)
+                if (asteroid_relative_position < collision_boundary && collided ==false)
                 {
-                    Destroy(gameObject);
+                    live.reduce_live(1);
+                    collided = true;
+                    collision_time= Time.time;
                 }
             }
         }
---
+++ Assets/live.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class live : MonoBehaviour
+{
+    [SerializeField] int lives_left;
+    [SerializeField] Image[] health;
+    [SerializeField] Sprite available_life;
+    [SerializeField] Sprite disabled_life;
+ 
+    public void reduce_live(int reducer)
+    {
+        lives_left -= reducer;
+        if(lives_left < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        for (int healthiterator = 0; healthiterator < health.Length; healthiterator++)
+        {
+            
+            if(healthiterator < lives_left)
+            {
+                health[healthiterator].sprite = available_life;
+            }
+            else
+            {
+                health[healthiterator].sprite = disabled_life;
+            }
+                
+        }
+    }
+}
